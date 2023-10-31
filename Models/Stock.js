99676c9f44a6ca444c6bb21484cdbfa2bb30d6b6
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-10-30 22:10:19 -0300

    create model and controller stock

---
+++ Models/Stock.js
@@ -0,0 +1,33 @@
+import mongoose from '../connection.js';
+const { Schema } = mongoose;
+
+const StockSchema = new Schema({
+    quantity: {
+        type: Number,
+        required: false,
+    },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+        unique: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Stock = mongoose.model('Stock', StockSchema);
+let error;
+try {
+  await Stock.save();
+} catch (err) {
+  error = err;
+}
+export default Stock;
+﻿
